08fa3a2149e219a63c3eee325471f899cef69282
Aakash <aakashmohole@gmail.com>
2024-03-08 11:24:01 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,redirect,url_for
+
+### WSGI Application
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return "Welcome to my Flask Web by aakash"
+
+
+@app.route('/members')
+def members():
+    return "Welcome to my world"
+
+@app.route('/success/<int:score>')
+def success(score):
+    return '<html><body><h1>The Person is Pass</h1></body></html>'
+ 
+ 
+@app.route('/fail/<int:score>')
+def fail(score):
+    return 'The person is failed by markes: '+str(score)
+
+
+
+## Result checker
+@app.route('/results/<int:marks>')
+def results(marks):
+    result = ""
+    if marks<50:
+        result = 'fail'
+    else: 
+        result = 'success'
+    #return result
+    return redirect(url_for(result, score = marks))
+
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
